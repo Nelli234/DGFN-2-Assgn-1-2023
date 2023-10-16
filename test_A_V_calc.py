3b4c3847df243b20de70eae949e2a0a9de5c64a2
Nelli234 <145477248+Nelli234@users.noreply.github.com>
2023-10-16 18:12:34 -0400

    Add files via upload

--- test_A_V_calc.py
+++ test_A_V_calc.py
@@ -12,3 +12,8 @@ def test_SQUARE_AREA():#test for area of a square
     assert SQUARE_AREA(2, 3) == 6
     assert SQUARE_AREA(1, 2) == 2
     assert SQUARE_AREA(0, 9) == 0
+    
+def CYL_VOL():#test for area of a square
+    assert CYL_VOL(2, 6) == 12
+    assert CYL_VOL(4, 4) == 16
+    assert CYL_VOL(0, 10) == 0
